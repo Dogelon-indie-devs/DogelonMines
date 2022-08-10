f2f3d3d7b527f6657a7e043dd9eb9d277bad4251
CryptoTangerine <crypto.tangerine@gmail.com>
2022-08-10 20:59:29 +0200

    initial commit

---
+++ Unit1.pas
@@ -0,0 +1,37 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Graphics,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Dialogs,
+  FMX.StdCtrls,
+  FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    Header: TToolBar;
+    Footer: TToolBar;
+    HeaderLabel: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
