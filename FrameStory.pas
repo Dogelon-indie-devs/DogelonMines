dc3cdaa1a89be484af08e8dbf5731b9778490af2
CryptoTangerine <crypto.tangerine@gmail.com>
2022-08-13 11:34:39 +0200

    storyboard finished

--- FrameStory.pas
+++ FrameStory.pas
@@ -17,7 +17,17 @@ type
     Text3: TText;
     ImageList1: TImageList;
     Image1: TImage;
+    SlidesRectangle: TRectangle;
+    NextButton: TRectangle;
+    ColorAnimation1: TColorAnimation;
+    ShadowEffect1: TShadowEffect;
+    Text1: TText;
+    Text2: TText;
+    StoryRectangle: TRectangle;
+    Text4: TText;
+    Text5: TText;
     procedure BackRectangleClick(Sender: TObject);
+    procedure NextButtonClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -33,17 +43,26 @@ uses
 
 procedure TStoryFrame.BackRectangleClick(Sender: TObject);
 begin
-  with Sender as TRectangle do
-    begin
-      var next_image:= ImageList1.Source[Tag].MultiResBitmap[0];
-      Image1.MultiResBitmap[0].Assign(next_image);
-      tag:= tag + 1;
-    end;
-
-  exit;
-
   MainForm.MainRectangle.Visible := True;
   MainForm.StoryRectangle.Visible := False;
+end;
+
+procedure TStoryFrame.NextButtonClick(Sender: TObject);
+begin
+  with Sender as TRectangle do
+    begin
+      if tag <= 9 then
+        begin
+          var next_image:= ImageList1.Source[Tag].MultiResBitmap[0];
+          Image1.MultiResBitmap[0].Assign(next_image);
+          tag:= tag + 1;
+        end
+      else
+        begin
+          Image1.Opacity:= 0;
+          StoryRectangle.Opacity:= 1;
+        end;
+    end;
 end;
 
 end.
